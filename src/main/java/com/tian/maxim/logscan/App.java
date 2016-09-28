2ffaa774c81a2b395b8a901a4330e2191a9fb029
tianmaxim <tian.liu@maximintegrated.com>
2016-09-28 15:11:36 -0700

    ready for first run test

--- src/main/java/com/tian/maxim/logscan/App.java
+++ src/main/java/com/tian/maxim/logscan/App.java
@@ -1,6 +1,7 @@
 package com.tian.maxim.logscan;
 
 import java.util.HashSet;
+import java.util.Hashtable;
 
 /**
  * Hello world!
@@ -11,16 +12,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        HashSet<LogErrorPattern> my_error_bank = new HashSet<LogErrorPattern>();
-
+//        HashSet<LogErrorPattern> my_error_bank = new HashSet<LogErrorPattern>();
+        Hashtable<String, LogErrorPattern> my_error_bank = new Hashtable<String, LogErrorPattern>();
         runPassMessage my_wrong_message = new runPassMessage();
 
-        my_error_bank.add(my_wrong_message);
+        my_error_bank.put(my_wrong_message.getPattern(), my_wrong_message);
 
-        PatternMatching my_pattern_Matching = new PatternMatching(my_error_bank);
-
-        System.out.println(my_pattern_Matching.isMatch());
-
-        System.out.println(args[0]);
+        if (args.length > 0) {
+            PatternMatching my_pattern_Matching = new PatternMatching(my_error_bank, args[0]);
+            System.out.println(my_pattern_Matching.isMatch());
+            System.out.println(args[0] + "   " + args[1]);
+        } else {
+            System.out.println("Please enter the log file path!!");
+            System.out.println(args[0]);
+        }
     }
 }
--- src/main/java/com/tian/maxim/logscan/PatternMatching.java
+++ src/main/java/com/tian/maxim/logscan/PatternMatching.java
@@ -1,18 +1,53 @@
 package com.tian.maxim.logscan;
 
-import java.util.HashSet;
+import sun.rmi.runtime.Log;
 
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 /**
  * Created by Tian Liu on 9/20/16.
  */
 public class PatternMatching {
-    private HashSet<LogErrorPattern>_error_message_bank;
+    private Hashtable<String, LogErrorPattern> _error_message_bank;
+    private PriorityQueue<LogErrorPattern> _reportLog;
+    private String _filepath;
 
-    PatternMatching(HashSet<LogErrorPattern> error_message_bank) {
+    PatternMatching(Hashtable<String, LogErrorPattern> error_message_bank, String filepath) {
+
         this._error_message_bank = error_message_bank;
+        this._filepath = filepath;
     }
 
     public int isMatch() {
+        File file = new File(_filepath);
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            //now read the file line by line...
+            int lineNum = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lineNum++;
+                if(this._error_message_bank.containsKey(line)) {
+                    System.out.println("ho hum, i found it on line " +lineNum);
+                }
+            }
+        } catch(FileNotFoundException e) {
+            //handle this
+        }
         return 0;
-    } 
+    }
+
+    public void storeErrorLog(String errorLog){
+
+    }
+
+    public void printErrorLog(){
+
+    }
 }
